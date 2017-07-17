3c26ac84d02acb1dd53c98078814c16c0f28fd31
Miroshnichenko Yaroslav <miroshnichenkoo19@gmail.com>
2017-07-17 16:31:32 +0300

    Рефракторинг

--- app/Adapters/Chunks/Chunk.php
+++ app/Adapters/Chunks/Chunk.php
@@ -3,12 +3,36 @@
 namespace App\Adapters\Chunks;
 
 /**
- * Класс, в котором реализован метод . //TODO метод
+ * Класс, в котором реализован метод fillJson.
  */
 abstract class Chunk
 {
-    public function formationJson()
-    {
-        //TODO формируется json из массива, кроме $masFlags
-    }
+    /**
+     *  Адаптер для чанка(умного элемента), который формирует json,
+     *  записывая данные в БД.
+     *
+     * @param array $options
+     *
+     * @return void
+     */
+    abstract public static function fillJson(array $options): void;
+
+    /**
+     * Переводит значение в двоичную строку, разбивает строку по символу,
+     * переворачивает массив.
+     *
+     * @param string $properties
+     *
+     * @return array
+     */
+    abstract public static function complementArray(string $properties): array;
+
+    /**
+     * Формирует ассоциативный массив.
+     *
+     * @param array $properties
+     *
+     * @return array
+     */
+    abstract public static function reformatProperties(array $properties): array;
 }
