ae83c87f2c8f2ad3591aef8b15e2c4097fcdda81
Pavel Gontarenko <pavgont@gmail.com>
2017-07-20 14:56:22 +0300

    - исправил тип данных в исключении

--- app/Adapters/Exceptions/ChunkTypeDoesNotExistException.php
+++ app/Adapters/Exceptions/ChunkTypeDoesNotExistException.php
@@ -5,20 +5,20 @@ namespace App\Adapters\Exceptions;
 use InvalidArgumentException;
 use Throwable;
 
-class ChunkIdDoesNotExistException extends InvalidArgumentException
+class ChunkTypeDoesNotExistException extends InvalidArgumentException
 {
     /**
-     * ChunkIdDoesNotExistException constructor.
+     * ChunkTypeDoesNotExistException constructor.
      *
-     * @param string $id
+     * @param int $type
      * @param int $code
      * @param Throwable|null $previous
      * @internal param string $message
      */
-    public function __construct(string $id, $code = 0, Throwable $previous = null)
+    public function __construct(int $type, $code = 0, Throwable $previous = null)
     {
         parent::__construct(
-            sprintf('Chunk with such an id = "%s" does not exist.', $id),
+            sprintf('Chunk with such a type = "%s" does not exist.', $type),
             $code,
             $previous
         );
