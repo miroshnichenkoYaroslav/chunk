fd94166e6c0782ea096a593d1e950fba6736e0da
Pavel Gontarenko <pavgont@gmail.com>
2017-07-24 21:27:04 +0300

    - добавил описание свойств объекта

--- app/ReChunk.php
+++ app/ReChunk.php
@@ -4,6 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property int $body
+ * @property int $site_id
+ * @property int $type
+ */
 class ReChunk extends Model
 {
     /**
